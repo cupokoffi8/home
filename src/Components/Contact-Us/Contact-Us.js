8676aabd54543074d9d94f048875b935fce2d33f
Alex <cupokoffi@gmail.com>
2021-08-01 20:35:00 -0400

    Added Folder for pages

---
+++ src/Components/Contact-Us/Contact-Us.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar'
+
+function ContactUs() {
+  return (
+    <div className="contact-us">
+      <Navbar />
+    </div>
+  );
+}
+
+export default ContactUs;
