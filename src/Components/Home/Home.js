1e65e2e7a6d265eec1c0310f9e5d8ef489a94da5
Alex <cupokoffi@gmail.com>
2021-08-07 14:00:02 -0400

    Uh

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
     return (
       <>
-        <h1 className='welcome' style="text-align: center; display: block; margin-left: auto; margin-right: auto;">Welcome</h1>
+        <h1 className='welcome'>Welcome</h1>
       </>
     );
   }
