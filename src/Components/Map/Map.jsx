13e1cd8e573d1ee982ab11531d4a4d990607c201
Alex <cupokoffi@gmail.com>
2021-08-07 17:02:04 -0400

    ll

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -53,7 +53,6 @@ export default function Map() {
         options={options}
         onLoad={onMapLoad}
       >
-        <Link to="https://www.google.com/maps/place/American+Dragon+International+Fine+Art+Gallery/@40.0483379,-76.3080947,17z/data=!3m1!4b1!4m5!3m4!1s0x89c62518e5cd1419:0x4c549ed6537c2a4!8m2!3d40.0483338!4d-76.3059007" className="mLink">
           <Marker 
             className="markerIcon" 
             position={{
@@ -61,13 +60,13 @@ export default function Map() {
               lng: -76.305810
             }}
             icon={{
+              to: "https://www.google.com/maps/place/American+Dragon+International+Fine+Art+Gallery/@40.0483379,-76.3080947,17z/data=!3m1!4b1!4m5!3m4!1s0x89c62518e5cd1419:0x4c549ed6537c2a4!8m2!3d40.0483338!4d-76.3059007",
               url: "https://lh3.googleusercontent.com/pw/ACtC-3eoIMEHvHhFrEnGjWsWYKk4bD8TY09mCUBZzBlb_erlpah5zoL710O_u2u_u5jb9e-S1B-uLXALiSlbR5bVhFO2WpkudlwXZg-74-ZRQYp7OQW5NpPIdTNCccg_pl2iAO8xU9grdN8-iWeohnW8NtJK=w95-h127-no?authuser=2", 
               scaledSize: new window.google.maps.Size(25, 30),
               origin: new window.google.maps.Point(0, 0), 
               anchor: new window.google.maps.Point(20, 20) 
             }}
           /> 
-          </Link> 
       </GoogleMap>
     </div>
   );
