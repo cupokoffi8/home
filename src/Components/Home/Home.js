0b2f56cd6dfed5cea7653c7f5155b5cf9e5ab00b
Alex <cupokoffi@gmail.com>
2021-08-07 16:03:38 -0400

    Red

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,7 +4,7 @@ import "./Home.css";
 export default function Home() {
     return (
       <>
-        <h1 className='welcome' id="uh" style="color: red;">Welcome to</h1>
+        <h1 className='welcome' id="uh">Welcome to</h1>
         <h1 className='american'>American Dragon International Fine Art Gallery</h1>
       </>
     );
