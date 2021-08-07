8b375be5cf859681afb5ab0d34e9b92d2d8b3f74
Alex <cupokoffi@gmail.com>
2021-08-07 09:04:23 -0400

    Image works

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,8 +4,6 @@ export default function Home() {
     return (
       <>
         <h1 className='welcome'>Welcome</h1>
-          <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.medicalnewstoday.com%2Farticles%2F322868&psig=AOvVaw30AbSc2Xqctmr9iqTxjcrh&ust=1628427735246000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCPCd9pn8nvICFQAAAAAdAAAAABAD'
-          /> 
       </>
     );
   }
