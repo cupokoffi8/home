1f02cb05c7cb3611cbf6b7d6c3e832a5f0831e20
Alex <cupokoffi@gmail.com>
2021-08-07 17:19:40 -0400

    words

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,9 @@ import Map from "../Map/Map";
 export default function Home() {
     return (
       <>
-        <h1 className='welcome' id="uh">Welcome to</h1>
+        <h1 className="magAndMyst">"Magnificent And Mysterious</h1>
+        <h1 clessName="currentExhibition">Art Exhibition</h1>
+        <h1 className='welcome' id="uh">Welcome To</h1>
         <h1 className='american'>American Dragon International Fine Art Gallery</h1>
         <Map /> 
       </>
