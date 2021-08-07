147c8800e1641aa90d370c1a2d1d73e002cae526
Alex <cupokoffi@gmail.com>
2021-08-07 15:52:54 -0400

    american

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,7 +4,8 @@ import "./Home.css";
 export default function Home() {
     return (
       <>
-        <h1 className='welcome' id="uh">Welcome</h1>
+        <h1 className='welcome' id="uh">Welcome to</h1>
+        <h1 className='american'>American Dragon International Fine Art Gallery</h1>
       </>
     );
   }
