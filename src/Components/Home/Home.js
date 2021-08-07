c818bf039118d9301b43d67bc4ed0bc7b2ea8b7c
Alex <cupokoffi@gmail.com>
2021-08-07 14:01:52 -0400

    uhhhh

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import "./Home.css"; 
 
 export default function Home() {
     return (
       <>
-        <h1 className='welcome'>Welcome</h1>
+        <h1 className='welcome' id="uh">Welcome</h1>
       </>
     );
   }
