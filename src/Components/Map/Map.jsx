e3307ffc9d6438c104ff02951befa0e910229bb8
Alex <cupokoffi@gmail.com>
2021-08-07 17:07:25 -0400

    buttON

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -61,13 +61,14 @@ export default function Map() {
               lng: -76.305810
             }}
             icon={{
-              to: "https://www.google.com/maps/place/American+Dragon+International+Fine+Art+Gallery/@40.0483379,-76.3080947,17z/data=!3m1!4b1!4m5!3m4!1s0x89c62518e5cd1419:0x4c549ed6537c2a4!8m2!3d40.0483338!4d-76.3059007",
               url: "https://lh3.googleusercontent.com/pw/ACtC-3eoIMEHvHhFrEnGjWsWYKk4bD8TY09mCUBZzBlb_erlpah5zoL710O_u2u_u5jb9e-S1B-uLXALiSlbR5bVhFO2WpkudlwXZg-74-ZRQYp7OQW5NpPIdTNCccg_pl2iAO8xU9grdN8-iWeohnW8NtJK=w95-h127-no?authuser=2", 
               scaledSize: new window.google.maps.Size(25, 30),
               origin: new window.google.maps.Point(0, 0), 
               anchor: new window.google.maps.Point(20, 20) 
             }}
-          /> 
+          > 
+          <button className="buutON" href="https://www.google.com/maps/place/American+Dragon+International+Fine+Art+Gallery/@40.0483379,-76.3080947,17z/data=!3m1!4b1!4m5!3m4!1s0x89c62518e5cd1419:0x4c549ed6537c2a4!8m2!3d40.0483338!4d-76.3059007" />
+          </Marker>
       </GoogleMap>
     </div>
   );
