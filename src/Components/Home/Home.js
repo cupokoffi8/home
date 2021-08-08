30aaf18c33293a87349c4570ab1044d5df2327df
Alex <cupokoffi@gmail.com>
2021-08-08 09:54:47 -0400

    num

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -11,9 +11,9 @@ export default function Home() {
         <h1 className='american'>American Dragon International Fine Art Gallery</h1>
 
         <div className="reachOut">
-          <button id="addy">614 N Duke Street, Lancaster, PA 17602</button>
-          <p>| 917-886-1701 |</p> 
-          <button id="addy" href="info@americandragonfineart.com">info@americandragonfineart.com</button>
+          <link id="addy">614 N Duke Street, Lancaster, PA 17602</link>
+          <p id="number">| 917-886-1701 |</p> 
+          <link id="addy" href="info@americandragonfineart.com">info@americandragonfineart.com</link>
         </div>
 
 
