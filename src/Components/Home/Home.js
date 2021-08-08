e9df79eb8b9be1506f91ac2eb5ad89170f06b9d7
Alex <cupokoffi@gmail.com>
2021-08-08 09:50:02 -0400

    tt

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,6 +9,15 @@ export default function Home() {
         <h1 clessName="currentExhibition" id="please">Art Exhibition</h1>
         <h1 className='welcome' id="uh">Welcome To</h1>
         <h1 className='american'>American Dragon International Fine Art Gallery</h1>
+
+        <div className="reachOut">
+          <button id="addy">614 N Duke Street, Lancaster, PA 17602</button>
+          <p>| 917-886-1701 |</p> 
+          <button id="addy" href="info@americandragonfineart.com">info@americandragonfineart.com</button>
+        </div>
+
+
+
         <Map /> 
       </>
     );
