46b290b77175f2745d1982067ac9b3232254a151
Alex <cupokoffi@gmail.com>
2021-08-09 19:44:39 -0400

    ll

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -12,25 +12,18 @@ export default function Home() {
         <h1 className='welcome' id="uh">Welcome To</h1>
         <h1 className='american'>American Dragon International Fine Art Gallery</h1>
 
-        <div id="info"> 
-          <div id="theDiv1">
-          <FontAwesomeIcon icon={faCoffee} />
+        <div id="info">
           <a id="addy" href="https://www.google.com/maps/place/American+Dragon+International+Fine+Art+Gallery/@40.0483338,-76.3059007,15z/data=!4m5!3m4!1s0x0:0x4c549ed6537c2a4!8m2!3d40.0483496!4d-76.3057639">
             614 N. Duke Street, Suite One, Lancaster, PA 17602
             </a>
-            </div>
-            <div id="theDiv2">
-              <FontAwesomeIcon icon={faCoffee} />
+
             <a id="addy">
                    917-886-1701      
             </a>
-            </div>
-            <div id="theDiv3">
-              <FontAwesomeIcon icon={faCoffee} />
+
             <a id="addy"> 
               info@americandragonfineart.com
             </a>
-            </div>
           </div>
 
 
