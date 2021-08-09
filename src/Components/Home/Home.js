11e8bbcf246e4cc484ef6e40e299a9c0629eeaa4
Alex <cupokoffi@gmail.com>
2021-08-09 19:26:36 -0400

    ll

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Home.css"; 
 import Map from "../Map/Map";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home() {
     return (
