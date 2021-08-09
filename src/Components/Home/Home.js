029e78b84eb5f2deaee2b0c68ff08a226982af17
Alex <cupokoffi@gmail.com>
2021-08-09 18:39:41 -0400

    BUTTON

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,8 +10,9 @@ export default function Home() {
         <h1 className='welcome' id="uh">Welcome To</h1>
         <h1 className='american'>American Dragon International Fine Art Gallery</h1>
 
-        <button id="addy">614 North Duke Street, Lancaster, PA 17602</button>
-
+        <form action="www.youtube.com">
+          <button id="addy" type="submit">614 North Duke Street, Lancaster, PA 17602</button>
+        </form>
 
 
         <Map /> 
