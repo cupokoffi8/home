57ae13b438f7ab2ab065d5e57696e28f2cdbf9f9
Alex <cupokoffi@gmail.com>
2021-08-09 19:03:14 -0400

    info

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -10,15 +10,17 @@ export default function Home() {
         <h1 className='welcome' id="uh">Welcome To</h1>
         <h1 className='american'>American Dragon International Fine Art Gallery</h1>
 
-        <a id="addy" href="https://www.google.com/maps/place/American+Dragon+International+Fine+Art+Gallery/@40.0483338,-76.3059007,15z/data=!4m5!3m4!1s0x0:0x4c549ed6537c2a4!8m2!3d40.0483496!4d-76.3057639">
-          614 N. Duke Street, Suite One, Lancaster, PA 17602
-          </a>
-          <a id="addy">
-            |    917-886-1701    |
-          </a>
-          <a id="addy" href="info@americandragonfineart.com"> 
-            info@americandragonfineart.com
-          </a>
+        <div id="info"> 
+          <a id="addy" href="https://www.google.com/maps/place/American+Dragon+International+Fine+Art+Gallery/@40.0483338,-76.3059007,15z/data=!4m5!3m4!1s0x0:0x4c549ed6537c2a4!8m2!3d40.0483496!4d-76.3057639">
+            614 N. Duke Street, Suite One, Lancaster, PA 17602
+            </a>
+            <a id="addy">
+              |    917-886-1701    |
+            </a>
+            <a id="addy" href="info@americandragonfineart.com"> 
+              info@americandragonfineart.com
+            </a>
+          </div>
 
 
         <Map /> 
