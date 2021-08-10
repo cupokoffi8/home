35af9a875d63d87bca2991f5138a7ce83090a971
Alex <cupokoffi@gmail.com>
2021-08-09 20:21:47 -0400

    ll

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -37,6 +37,7 @@ export default function Home() {
             <p>Saturday:  By Appointment Only</p>
             <p>Sunday:  By Appointment Only</p>
           </div>
+        <hr />
       </>
     );
   }
