891e0d673f3af422456470cae4c6ad00f317137a
Alex <cupokoffi@gmail.com>
2021-08-12 20:26:23 -0400

    ll

--- src/Components/Artists/Aparna-Banerjee/Aparna-Banerjee.js
+++ src/Components/Artists/Aparna-Banerjee/Aparna-Banerjee.js
@@ -1,11 +1,22 @@
 import React from 'react'; 
 import './Aparna-Banerjee.css'; 
+import Map from '../../Map/Map'; 
 
 function AparnaBanerjee() {
     return (
         <div>
           <div className="AparnaBanerjee">
-            <h1 className="aparna-banerjee">Aparna Banerjee</h1>
+            <h1 className="artist">Aparna Banerjee</h1>
+            <hr /> 
+            <p id="art-paragraph">
+              The work of the outstanding Indian female artist Aparna Banerjee 
+              perfectly combines Eastern traditional art with Western modern genre. 
+              In her paintings, she can trace the footprints of contemporary genre 
+              masters and the Dunhuang aftertaste!  The source is naturally the 
+              traditional Buddhist culture of India!
+              </p>
+
+              <Map /> 
           </div>
         </div>
     );
