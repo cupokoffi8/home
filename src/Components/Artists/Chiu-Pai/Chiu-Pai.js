fda70467decbc2e5894bcd9c4a74f4791d6be46e
Alex <cupokoffi@gmail.com>
2021-08-13 20:21:22 -0400

    ll

--- src/Components/Artists/Chiu-Pai/Chiu-Pai.js
+++ src/Components/Artists/Chiu-Pai/Chiu-Pai.js
@@ -13,12 +13,30 @@ function ChiuPai() {
             {/* Artist Summary */}
 
             <p id="art-paragraph">
-              The work of the outstanding Indian female artist Aparna Banerjee 
-              perfectly combines Eastern traditional art with Western modern genre. 
-              In her paintings, she can trace the footprints of contemporary genre 
-              masters and the Dunhuang aftertaste!  The source is naturally the 
-              traditional Buddhist culture of India!
-              </p>
+            Chiu Pai was born in Taipei. She studied Fashion Design at the Garment Association, 
+            and studied painting with the famous painter Xu Min. Chiu Pai moved to Vancouver, 
+            British Columbia in 1999, and studied Western Painting at the Emily Carr University 
+            of Art and Design.
+              </p> 
+
+              <p id="art-paragraph-other">
+              She is a member of the International Chinese Artists Association, the Canadian Artists 
+              Association, the Chinese International Culture and Art Promotions Association, the World 
+              Association of Chinese Painting and Calligraphy, and the Vice President and Chief Curator 
+              of the Painting and Calligraphy Institute in Vancouver. 
+              </p> 
+
+              <h2 id="header">
+              Remarks from Chiu Pai:
+              </h2> 
+
+              <p id="art-paragraph-other">
+              “I use the oxymoron “ Awaken Dream “ to describe my paintings, as I envision the paintings. 
+              They are so real. People are intrigued and captivated by the “Awaken Dream” concept. I always 
+              wish those dreams can radiate a peaceful, cohesive, and serene atmosphere. The finished images 
+              should display a foggy and misty appearance, one that shifts away from detail. The accomplished 
+              painting should reflect my hazy emotion.”
+              </p> 
 
               {/* List of Paintings */} 
 
