cfc2be97d86472607924e8e98fbcc9f288166aea
Alex <cupokoffi@gmail.com>
2021-08-14 15:00:18 -0400

    Zhao Qing

--- src/Components/Artists/Zhao-Qing/Zhao-Qing.js
+++ src/Components/Artists/Zhao-Qing/Zhao-Qing.js
@@ -13,12 +13,22 @@ function ZhaoQing() {
             {/* Artist Summary */}
 
             <p id="art-paragraph">
-              The work of the outstanding Indian female artist Aparna Banerjee 
-              perfectly combines Eastern traditional art with Western modern genre. 
-              In her paintings, she can trace the footprints of contemporary genre 
-              masters and the Dunhuang aftertaste!  The source is naturally the 
-              traditional Buddhist culture of India!
-              </p>
+            Zhao Qing was born in 1936. She was the Vice Chairman of the Chinese Dancers Association, and the 
+            daughter of the famous film actor Zhao Dan. She has many classic dance works, but she was injured 
+            at the age of 54. She began to learn to paint and her talent is innate. Every oil painting of her 
+            life has vitality. It appears to be a jumping note, which makes people happy. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              In September of 2013, the United States Guggenheim Museum, the Lincoln Art Center, and other famous 
+              American Art Institutions jointly invited Ms. Zhao Qing to hold painting exhibitions in the United States. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              “As long as you want to learn to paint, you should not care about your age and try to be your best. Time 
+              is not a problem”. Ms. Zhao Qing is nearly 60 years old. She started a new artistic life with painting. 
+              This exhibition gives more people confidence and begins to experiment with art. 
+              </p> 
 
               {/* List of Paintings */} 
 
