56462dc42781102ea45f594bd70565e201f63529
Alex <cupokoffi@gmail.com>
2021-08-13 22:01:06 -0400

    ll

--- src/Components/Artists/Xiao-Li/Xiao-Li.js
+++ src/Components/Artists/Xiao-Li/Xiao-Li.js
@@ -41,6 +41,24 @@ function XiaoLi() {
                 </li>
               </ul> 
 
+              <p id="art-paragraph-other">
+              He became the first mainland artist to create residential gardens 
+              at the invitation of the Guilin Yuzi Paradise International Art Park. 
+              For nearly four years, he created a series of large scale wood carvings, 
+              such as “ Big Dream ”, “ Late Night ”, and “ Source”. Xiao Li was also 
+              invited to discuss, shape, and trial produce for the opening and closing 
+              ceremonies of the Beijing Olympic Games. He played a decisive role in the 
+              function and shape of lifting props for the successful realization of 
+              the “movable type“ performance projects. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              He is currently a Professor in the Sculpture Department of the Central Academy 
+              of Fine Arts, invited to be an expert of the Ministry of Culture, and a standing 
+              member of the China Wood Carving Professional Committee. He currently presides 
+              over the teaching of the wood sculpture studio at the academy. 
+              </p> 
+
               {/* List of Paintings */} 
 
               <img src="./beauty_1.jpeg" id="painting"></img>
