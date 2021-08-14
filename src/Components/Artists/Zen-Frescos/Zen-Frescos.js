55c033c951a6a10f57df44f87d3021c35705de4e
Alex <cupokoffi@gmail.com>
2021-08-14 08:48:12 -0400

    ll

--- src/Components/Artists/Zen-Frescos/Zen-Frescos.js
+++ src/Components/Artists/Zen-Frescos/Zen-Frescos.js
@@ -7,18 +7,235 @@ function ZenFrescos() {
 
             {/* Artist Name */} 
 
-            <h1 className="artist">Zen Frescos</h1>
+            <h1 className="artist">The Collection And The Appreciation of Fahai Zen Monastery Frescoes, Han, Zhijun</h1> 
             <hr /> 
 
             {/* Artist Summary */}
 
             <p id="art-paragraph">
-              The work of the outstanding Indian female artist Aparna Banerjee 
-              perfectly combines Eastern traditional art with Western modern genre. 
-              In her paintings, she can trace the footprints of contemporary genre 
-              masters and the Dunhuang aftertaste!  The source is naturally the 
-              traditional Buddhist culture of India!
-              </p>
+            Avatamsaka Sutra says: "There are five color flowers in Buddha's world. Through flowers, 
+            one can see a world in a single piece of flower, one can see a Buddha in a single piece 
+            of leaf." The venerable layperson Mr. Puchu Zhao once said: "There are Buddha’s worlds 
+            in Buddhist paintings. Buddhist paintings manifest Buddha’s worlds." Using artwork as 
+            expedient way to spread Buddha’s doctrine has more than 2000 years of history in China. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              As it is said in Buddhism: "Experience the nature of the mind through seeing phenomena. 
+              The nature of the mind cannot be perceived without the presence of the phenomena. 
+              ”Buddhism and the art are interrelated through mutual complement and enhancement. 
+              The extraordinary karmic bonds between the Buddhist Dharma and Chinese mural paintings 
+              brought Chinese Buddhist mural paintings to its culmination of charisma which reflect 
+              the majesty of the Buddhist world and perfection of the Avatamsaka Cosmos. Chinese 
+              Buddhist mural paintings glorified the essence of Buddhism, and the Buddhist 
+              transcendental wisdom elevated the glamour of Chinese Buddhist mural paintings to 
+              its highly respected height in the world.  Chinese Buddhist mural painting is the 
+              Prajna of art which annotates the emptiness wisdom of the reality in Buddhism.
+              </p> 
+
+              {/* -------------- */}
+              <img src="./beauty_1.jpeg" id="painting"></img>
+              <h5 id="caption">The Avalokitesvara with Water and Moon</h5> 
+              {/* -------------- */} 
+
+              <h3 id="little-title">
+              A、Fahai Zen Monastery Frescoes
+              </h3>
+
+              <p id="art-paragraph-other">
+              Fahai Zen Monastery Frescoes were painted by 15 royal painters including painting 
+              officer Fuqing Wan, Shu Wang, painter Ping Zhang, Yi Wang, Xing Gu, Yuan Li, Fu Pan, 
+              and Fulin Xu, who were appointed and supervised by royal eunuch Li, Tong, during 
+              period of Zhengtong reign year 4th to year 8th ( A.D. 1439-A. D.1443) of Ming Dynasty, 
+              in the main shrine hall of the royal zen monastery. They encompass:
+              </p> 
+
+              {/* Bullet Points */} 
+
+              <p id="art-paragraph-other">
+              Among them, the “The Avalokitesvara with Water and Moon” and the “indra and Brahma 
+              Prostrating Buddha and Protecting Dharma” are the most representative masterpieces. 
+              Fahai Zen Monastery Frescoes have notable Ming Dynasty hints with heritages from 
+              the Tang Dynasty, the 5 Dynasties, and the Song Dynasty. They exhibit exquisite 
+              subtlety, clear patterns, dazzling tone, and deep dharma philosophy through narrative 
+              plot, arrangement of the figures, image design of subjects and composition of elements. 
+              They integrate the metaphoric meaning and the Buddhist transcendental wisdom with the 
+              statues of the Buddha and Bodhisattva. These paintings convey the emptiness nature of 
+              all phenomenons. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              The frescoes cause people to experience the nature of mind directly. The frescoes merge 
+              symbols, landscapes, flowers, woods and stone with smooth lines and bright colors. 
+              Along with the rituals and disciplines of Buddhism, they are painted in the traditional 
+              Chinese meticulous painting method, which is characterized by delicate brushstrokes, 
+              close attention to details, brightly dotted colors and strict requirements in material 
+              selections. They fully reflect the distinguished elegance, royal grandeur, and 
+              meticulousness of the Imperial family. The Fahai Zen Monastery Frescoes are the aesthetic 
+              masterpieces among the noblest, the most matured, the most sophisticated Chinese Monastery 
+              Murals. The artistry of the murals reached its apogee by congregating the quintessence of 
+              Chinese monastery mural paintings through all periods. They generated a spectacular example 
+              of religious art in human art history.
+              </p> 
+
+              <p id="art-paragraph-other">
+              In 1993, antique appraisers, artists and religious leaders from Beijing evaluated Fahai 
+              Zen Monastery Frescoes in its historical value, religion, art and painting skills. Through 
+              comparison with the murals from Yuan Dynasty, Ming Dynasty, Qing Dynasty , experts 
+              collectively agreed that Fahai Zen Monastery Frescoes have their own merits, which could 
+              match the beauty of renaissance murals in Europe. They are among the most precious masterpieces 
+              in world mural art. Fahai Zen Monastery Frescoes are first-grade national treasures in China. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              When the late legendary Chinese mural painting expert Mr. Dingli Wang (1925-2009) led the 
+              restoration and conservation of Yongle Frescoes, he found many mistakes and immaturities in 
+              the paintings. However, after a comprehensive study, he could not find a single stroke of 
+              mistake in Fahai Zen Monastery Frescoes. The late remarkable Chinese meticulous painter 
+              Mr. Jiezi Pan said: “Among the all ancient mural paintings I ever saw, Fahai Zen Monastery 
+              Frescoes are the most detailed mural paintings that delimit details precisely.” He also 
+              composed a poem for Fahai Monastery Frescoes:
+              </p> 
+
+              <p id="art-paragraph-poem">
+              “Dunhuang Frescoes have both successful and unsuccessful brushstrokes among the marvelousness; 
+              the detailed illustration of Yongle Palace Frescos are the benchmarks for meticulous paintings. 
+              However, Fahai Monastery’s mural paintings are the most meticulous one of all the periods. 
+              No other painting can compare itself with the exquisite details and metaphoric meanings of 
+              Fahai Zen Monastery Frescoes. What else painting can compete itself with the mystically elegant 
+              significance of the Fahai Monastery Frescoes?” 
+              </p> 
+
+              {/* -------------- */}
+              <img src="./beauty_1.jpeg" id="painting"></img>
+              <h5 id="caption">Portrait of Manjusri</h5> 
+              {/* -------------- */} 
+
+              <p id="art-paragraph-other">
+              Chinese notable contemporary artists including Mr. Lingyun Qin, Mr. Weinuo Jin 
+              and Mr. Yongnian Xue also gave Fahai Monastery Frescoes high evaluations. Fahai 
+              Monastery Frescoes are treated as the highest achievement in Buddhist mural art 
+              after the exploration of Kizil Caves Grottoes and Kucha Grottoes of A.D. 3 to 
+              A.D. 4, for its exquisite, its solemnity, its magnificence, and its aesthetic 
+              values.
+              </p> 
+
+              <p id="art-paragraph"> 
+              The “The Avalokiteshvara with Water and Moon”  of Fahai Zen Monastery Frescoes 
+              is acclaimed as the most significant painting of Avalokiteshvara in both Buddhist 
+              and Art Communities.  Through the use of typical Buddhist elements, like crown, 
+              keyura, seamless heavenly garment, silk gauze, and luminous light, the frescoes 
+              symmetrically parallelled Sudhanakumara, Skanda Bodhisattva, Golden Haired Hou-the 
+              steed of Avalokitesvara, parrot. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              On the immense watery cloud background, the frescoes are spotted auspicious clouds, 
+              bamboo garden, red peony, red coral, magnolia tree, artocarpus tree, eupatorium flower, 
+              and treasure vase, symbolizing longevity mountain, symbolizing prosperity ocean, and 
+              clear spring water; all of those Buddhist elements bring out the evocative brightness 
+              and solemnity of Buddhist pure-land and the Buddhist meaning of“ the dependent existing 
+              of the heavens is not a truly existence; the true nature of the watery moon is emptiness”. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              By adopting the painting skills in Song Dynasty, Fahai Zen Monastery Frescoes attribute 
+              human characteristics and emotional charm to Avalokiteshvara. As a result of this effort, 
+              Avalokiteshvara has a heart-warming deportment, tranquil and compassionately amicable 
+              expression. The frescoes fully reflect the Buddhist teachings of transcendence wisdom and 
+              life liberation. Just looking at “The Avalokiteshvara with Water and Moon” silently, the 
+              painting will bring us into the Buddha’s world of emptiness and infinite compassion. That 
+              is a world of nonduality. It is incredible to find the similarities between the “The 
+              Avalokiteshvara with Water and Moon” of Fahai Zen Monastery Frescoes and the “Sistine Madonna  
+              of Sistine Chapel,  Italian Painter Raffaello Sanzio da Urbino of the High Renaissance. 
+              The painting of “The Avalokiteshvara with Water and Moon” is crowned the eastern “Sistine Madonna”. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              There are common figures like Buddha, Bodhisattva, yama, arahat, deva, Nāga, Yakṣ a, 
+              Gandharva, Asura, Garuḍa, Kiṁnara, Mahoraga, Buddhist venerables in Chinese traditional 
+              Buddhist paintings.”Indra and Brahma Prostrating Buddha and Protecting Dharma” depicts 
+              heavenly kings and Nagas leading celestial beings, like Yakṣa, Gandharva,Asura, Garuḍa, 
+              Kiṁnara,Mahoraga to Buddhist life releasing ceremony. In addition to the 26 celestial 
+              pure ones, there are totally 36 humanized celestial figures and 7 divine animal figures 
+              were painted on the eastern and the western wall of the main shrine hall. Some of them 
+              are ghost spirit, devi, mantra master, green lion, white elephant, leopard, boar, fox, 
+              golden toad, and purple golden dragon. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              The “Indra and Brahma Prostrating Buddha and Protecting Dharma” was composed by murals 
+              of the eastern wall and western wall. Standing face to face, Indra and Brahma lead 
+              celestial beings, ghost beings, female devas, mantra masters heading to puja. They are 
+              either in small groups, or greeting each other, or performing their supernatural power. 
+              All those components together result in a vigorous, magnificent, stunning and breathtaking 
+              scenario of mightiness. All figures are related but each figure has a unique expression 
+              to convey different identity, characteristic and quality. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              Through expression, appearance, posture, gesture, holding item, Dharma instrument, 
+              attire, accessory, color, auspicious cloud, celestial animal, grass and flower of 
+              each figure, the illustrations on Fahai Zhen Monastery Frescoes reflect the Buddhist 
+              stories and teachings with wish fulfilled blessing. Differentiated not only through 
+              holding item and body movement, each figure is characterized by its unique reaction 
+              for the same activity. 
+              </p> 
+
+              {/* -------------- */}
+              <img src="./beauty_1.jpeg" id="painting"></img>
+              <h5 id="caption">Portrait of Samantabhadra</h5> 
+              {/* -------------- */} 
+
+              <p id="art-paragraph-other">
+              For instance, Caturmahārājakayika ( heavenly king) and vajrapani both are Buddhist 
+              protectors with majestic appearance and great dignity. In the “Indra and Brahma 
+              Prostrating Buddha and Protecting Dharma”, the heavenly king is portrayed especially 
+              in his tenderness and sedateness. On the other hand, Vajrapani, the “wielder of the 
+              thunderbolt” in Buddhist liturgy is with wrathful looking. By humanizing heavenly 
+              beings with emotions and feministic personalities, the heartwarming murals are more 
+              closed to real-life situations. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              The ancient Chinese dialectical concepts in philosophy are appropriately interpreted 
+              through dramatically composed plot, symbolized appearance, majesty and compassion of 
+              the figures. The Fahai Zen Monastery Frescoes created great contributions to Chinese 
+              mural history. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              In term of painting process and techniques, Fahai Zen Monastery Frescoes have reached 
+              its culminating point with everlasting artistic power for it. They manifest the splendid 
+              and sumptuous essence of Chinese court painting, a thousand years of Dunhuang Frescoes, 
+              and Shanxi province’s Yongle Frescoes of the Yuan Dynasty in both appearance and composition. 
+              They have Gu, Kaizi’s brush lines of “Spring Silkworms Spitting Silk”, Cha, Zhongda’s 
+              “Wet-clothing Drawing”, and Wu Daozi’ s “Wu's Belt Floating in Wind". Mural paintings were 
+              colored with powdered gold. People can even see realistic strands of hair through its skilled 
+              brushstroke techniques like Lanye Miao, Tiexian Miao, Zhelu Miao, Zhanbi Miao, Gaoguyousi Miao, 
+              Dingtoushuwei Miao. From the overall perspective, it has magnificent rhythm, vivid contrast 
+              and bold floating lines. It demonstrates unique technique through multiple layers, texture and 
+              authentic genius. Additionally, it applied stringent techniques in material selection along 
+              with complicated procedures. It is impeccable and incomparable.
+              </p> 
+
+              <p id="art-paragraph-other">
+              The various values of the Fahai Zen Monastery Frescoes in religion, Buddhist teaching, and 
+              visual art exceed the original painting purpose. Fahai Zen Monastery Frescoes are the rare 
+              gems for Buddhist cultural studies. There are more values waiting there to be explored, to 
+              be inherited, to be brought out from monastery to people, from people to the world. 
+              </p> 
+
+
+
+
+
+              <p id="art-paragraph-other">
+              The ancient Chinese dialectical concepts in philosophy are appropriately interpreted 
+              through dramatically composed plot, symbolized appearance, majesty and compassion of 
+              the figures. The Fahai Zen Monastery Frescoes created great contributions to Chinese 
+              mural history. 
+              </p> 
 
               {/* List of Paintings */} 
 
