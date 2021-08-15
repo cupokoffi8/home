f25cd04ceb53117a75f38386568b3e78f95a851c
Alex <cupokoffi@gmail.com>
2021-08-14 23:50:56 -0400

    ll

--- src/Components/Artists/Artists/Artists.js
+++ src/Components/Artists/Artists/Artists.js
@@ -8,52 +8,52 @@ function Aritists() {
             <h1 className="artist">Artists</h1> 
             <hr /> 
 
-            <a id="link" href="/home/aparna-banerjee">
+            <a id="link" href="/home/#/aparna-banerjee">
             <img id="image" src="./beauty_2.jpeg"/> 
             <h5 id="caption">Aparna Banerjee</h5> 
             </a>
 
-            <a id="link" href="/home/apelles-zhou">
+            <a id="link" href="/home/#/apelles-zhou">
             <img id="image" src="./beauty_2.jpeg"/>
             <h5 id="caption">Apelles Zhou</h5> 
             </a>
 
-            <a id="link" href="/home/cai-jiang">
+            <a id="link" href="/home/#/cai-jiang">
             <img id="image" src="./beauty_2.jpeg"/>
             <h5 id="caption">Cai Jiang</h5> 
             </a>
 
-            <a id="link" href="/home/chiu-pai">
+            <a id="link" href="/home/#/chiu-pai">
             <img id="image" src="./beauty_2.jpeg"/>
             <h5 id="caption">Chiu Pai</h5> 
             </a>
 
-            <a id="link" href="/home/qui-huade">
+            <a id="link" href="/home/#/qui-huade">
             <img id="image" src="./beauty_2.jpeg"/>
             <h5 id="caption">Qui Huade</h5> 
             </a>
 
-            <a id="link" href="/home/su-zi">
+            <a id="link" href="/home/#/su-zi">
             <img id="image" src="./beauty_2.jpeg"/>
             <h5 id="caption">Su Zi</h5> 
             </a>
 
-            <a id="link" href="/home/xiao-li">
+            <a id="link" href="/home/#/xiao-li">
             <img id="image" src="./beauty_2.jpeg"/>
             <h5 id="caption">Xiao Li</h5> 
             </a>
 
-            <a id="link" href="/home/zen-frescos">
+            <a id="link" href="/home/#/zen-frescos">
             <img id="image" src="./beauty_2.jpeg"/>
             <h5 id="caption">Zen Frescos</h5> 
             </a>
 
-            <a id="link" href="/home/zhao-qing">
+            <a id="link" href="/home/#/zhao-qing">
             <img id="image" src="./beauty_2.jpeg"/>
             <h5 id="caption">Zhao Qing</h5>
             </a> 
 
-            <a id="link" href="/home/zhen-zhong-duan"> 
+            <a id="link" href="/home/#/zhen-zhong-duan"> 
             <img id="image" src="./beauty_2.jpeg"/>
             <h5 id="caption">Zhen-Zhong Duan</h5> 
             </a>
