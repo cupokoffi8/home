7028df9c0678a85983e84db366e676130ba750ea
Alex <cupokoffi@gmail.com>
2021-08-15 14:30:11 -0400

    Finished Magnificent and Mysterious Art Exhibition

--- src/Components/Exhibitions/Mag-And-Myst/Mag-And-Myst.js
+++ src/Components/Exhibitions/Mag-And-Myst/Mag-And-Myst.js
@@ -17,6 +17,37 @@ function MagAndMyst() {
 
             <img src="./2.jpeg" id="image-thing" /> 
 
+            <img src="./3.jpeg" id="image-thing" /> 
+
+            <p id="paragraph" >
+            Senior international curator Lena Liu introduced 87 works of art for the exhibition. The art is from various 
+            painters from China, the United States, India, and Canada. Among them, the landscape paintings of the Chinese 
+            painter Duan Zhenzhong are the featured works. Mr.Duan's works have repeatedly won positive results in Sotheby's 
+            auctions.This exhibition reproduces the oriental ink charm, highlighting the beauty of China's Jiangnan water 
+            villages, and the gorgeous and ancient buildings and streetscapes in the West. The buildings in the landscape 
+            paintings record the history and culture of different countries, and also demonstrate the beauty of that era. 
+            </p> 
+
+            <img src="./6.jpeg" id="image-thing" /> 
+
+            <p id="paragraph" >
+            The opening reception began with guitar and vocal talents performed by American artist Jeff Moore.
+            </p> 
+
+            <p id="paragraph" >
+            The soothing music, artworks full of "gorgeous and mysterious" colors, fine wines, and rich Chinese food, 
+            created a wonderful atmosphere.The guests also expressed great interest in the gallery works. 
+            </p> 
+
+            <img src="./4.jpeg" id="image-thing" /> 
+
+            <img src="./5.jpeg" id="image-thing" /> 
+
+            <p id="paragraph" >
+            The exhibition is co-sponsored by Asian American Culture Media Group and American Dragon International 
+            Fine Art Gallery. The current exhibition will end on August 23. 
+            </p> 
+
           </div>
         </div>
     );
