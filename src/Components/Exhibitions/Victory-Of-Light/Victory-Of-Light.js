da69f94b8f87b0e67e1a6e2120c78bbdc5df0932
Alex <cupokoffi@gmail.com>
2021-08-17 18:56:35 -0400

    ll

--- src/Components/Exhibitions/Victory-Of-Light/Victory-Of-Light.js
+++ src/Components/Exhibitions/Victory-Of-Light/Victory-Of-Light.js
@@ -5,7 +5,35 @@ function VictoryOfLight() {
     return (
         <div>
           <div className="Victory-Of-Light">
-            <h1 className="exhibition">Victory of Light Exhibition</h1>
+            <h1 className="exhibition">Victory of Light Exhibition</h1> 
+
+            <h6 id="head">
+              Organizers: 
+            </h6>
+            <p id="points">
+              Asian American Culture Media Group
+            </p>
+            <p id="points">
+              American Dragon International Fine Art 
+            </p>
+
+            <h6 id="head">
+              Contractors: 
+            </h6>
+            <p id="points">
+              The Wall Street Art Space
+            </p> 
+
+            <h6 id="head">
+              Curators: 
+            </h6>
+            <p id="points">
+              Lena Liu 
+            </p>
+            <p id="points">
+              Apelles Zhou 
+            </p>
+
           </div>
         </div>
     );
