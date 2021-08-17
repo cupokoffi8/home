808213089ec0c26c6a2554fa988093feb3d0eea9
Alex <cupokoffi@gmail.com>
2021-08-17 19:11:05 -0400

    ll

--- src/Components/Exhibitions/Victory-Of-Light/Victory-Of-Light.js
+++ src/Components/Exhibitions/Victory-Of-Light/Victory-Of-Light.js
@@ -32,8 +32,33 @@ function VictoryOfLight() {
             </p>
             <p id="points">
               Apelles Zhou 
-            </p>
+            </p> 
 
+            <h2 id="header">
+              OPENING RECEPTION, FEBRUARY 8, 2020; 3:30 P.M. 
+              </h2> 
+              <p id="art-paragraph-other">
+              Fifteen artists from nine countries will showcase their engaging photographs, paintings, 
+              and installation / sculpture works of art that will be included in the next exhibition, 
+              the International Light and Shadow Art Exhibition, 2020.   This exhibition will take place 
+              from February 8, 2020 through March 9, 2020.  The earth will transition to the Aquarius 
+              Era during this Wall Street Art Space exhibition. 
+              </p> 
+              <p id="art-paragraph-other">
+              Asian American Culture Media Group and American Dragon International Fine Art, LLC, have 
+              achieved great success due to the integration of these two institutions, featuring a Cultural 
+              Education Center and Art Gallery.  We use this business model to complement economic and 
+              cultural advantages.  Our perfect location on Wall Street and The Trinity can represent material 
+              and spiritual harmony.  Some of our clients are from the financial industry, including Mr. Stephen 
+              Rockefeller.  These clients participate on a regular basis in support of our Art Exhibitions, 
+              and we have cultivated a large group of collectors from this social circle. 
+              </p> 
+              <p id="art-paragraph-other">
+              At the opening reception on Saturday, February 8, 2020, we will host a concert to further integrate 
+              the event with our cultural values, as well as help to attract more clients and collectors. The 
+              exhibition will include 40 works from the talented artists. 
+              </p> 
+              
           </div>
         </div>
     );
