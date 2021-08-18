9aff4cb20810c6b6a3fe8d2248b9395361daddb7
Alex <cupokoffi@gmail.com>
2021-08-18 19:20:51 -0400

    ll

--- src/Components/Exhibitions/Art-Basel/Art-Basel.js
+++ src/Components/Exhibitions/Art-Basel/Art-Basel.js
@@ -5,7 +5,39 @@ function ArtBasel() {
     return (
         <div>
           <div className="Art-Basel">
-            <h1 className="exhibition">Art Basel</h1>
+            <h1 className="exhibition">Art Basel Exhibition, Miami, Florida</h1> 
+            <hr /> 
+
+            <p id="art-paragraph-other">
+            Yachang Art Network is the world’s most important Chinese Art Internet brand, with over 2  
+            million professional members and 8 million daily page reviews. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              Artron’s Art Headlines ( artron.net ) reported that the well known curator Lena Liu launched 
+              the work of artist Qiu Zihao at Art Basil.  His exhibition at Spectrum Miami attracted the 
+              attention of many collectors. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              Inspired by the works of Paul Klee and Wassily Kandinsky, Howard ( Qui Zihao ) delved deeply 
+              into the relationship between music and art.  This journey has led him to synthesize fine art 
+              with music, to create an innovative system of paintings that recognize rhythm and melodies. 
+              With this system, he is making striking paintings, each containing a musical composition. By 
+              combining the visual and the aural, he provides a novel way to dynamically engage one’s senses 
+              in enjoying works of paintings and melodies. 
+              </p> 
+
+              <p id="art-paragraph-other">
+              Chinese artists at Art Basel Miami 2019
+              </p> 
+
+              <a href="https://news.artron.net/20191214/n1067043.html">
+              <p id="art-paragraph-other">
+              https://news.artron.net/20191214/n1067043.html
+              </p> 
+              </a> 
+              
           </div>
         </div>
     );
