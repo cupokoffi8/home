7fa9b3545db4b2a868a22532c8a88ac2e00c1328
Alex <cupokoffi@gmail.com>
2021-08-21 10:40:44 -0400

    ll

--- src/Components/Exhibitions/Magnificent/Magnificent.js
+++ src/Components/Exhibitions/Magnificent/Magnificent.js
@@ -22,6 +22,17 @@ function Magnificent() {
             <h1 id="ndot"> 
               The Collection and Appreciation of Fahai Zen Monastery Frescoes 
             </h1> 
+
+            <h6 id="caption-small">
+              Opening at the club of CEO Masayoshi Son, a billionaire from the SoftBank Group in Tokyo, Japan (an investor in Alibaba)  
+            </h6> 
+
+            <p id="art-paragraph-other">
+            The art exhibition in Japan was a great success. Japanese museums, art academies, and galleries rushed 
+            to make an appointment for the next exhibition. The exhibition of Nagasaki, Japan’s Fahai Temple Mural, 
+            will be opened in late July. The “ China Cultural Center “ exhibition in Tokyo, “ Exhibition of Beijing 
+            Fahai Temple Murals”, was exhibited at the end of the year. 
+            </p>
           </div>
         </div>
     );
