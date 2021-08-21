3547700ea95fd71edad921a9fffb3fe504f6be55
Alex <cupokoffi@gmail.com>
2021-08-20 21:22:14 -0400

    ll

--- src/Components/Exhibitions/Magnificent/Magnificent.js
+++ src/Components/Exhibitions/Magnificent/Magnificent.js
@@ -5,7 +5,15 @@ function Magnificent() {
     return (
         <div>
           <div className="Magnificent">
-            <h1 className="exhibition">Magnificent Murals</h1>
+            <h1 className="exhibition">Chinese Murals at the Tokyo Exhibition in Japan
+              Reproduction of the Magnificent Murals of 600 Years Ago</h1> 
+              <hr /> 
+
+              <h5 id="caption">
+              June 26 - July 3, 2016 
+            </h5> 
+
+            <img id="image-thing" src="M1.jpeg" /> 
           </div>
         </div>
     );
