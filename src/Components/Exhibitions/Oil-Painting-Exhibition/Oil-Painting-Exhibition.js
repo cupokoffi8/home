7f5f77a3250062ea394d8907edf5c4951b37af14
Alex <cupokoffi@gmail.com>
2021-08-22 11:35:38 -0400

    ll

--- src/Components/Exhibitions/Oil-Painting-Exhibition/Oil-Painting-Exhibition.js
+++ src/Components/Exhibitions/Oil-Painting-Exhibition/Oil-Painting-Exhibition.js
@@ -5,8 +5,12 @@ function PleaseWork() {
     return (
         <div>
           <div className="Oil-Painting-Exhibition">
-            <h1 className="exhibition">Oil Painting Exhibition</h1> 
+            <h1 className="exhibition">Oil Painting Exhibition in Jeju Island, South Korea</h1> 
             <hr /> 
+
+            <h5 id="caption">
+              April 22- April 26, 2014 
+            </h5>
 
             <div id="the-pictures">
               <img id="image-thing-at-uh" src="./OP1.jpeg" /> 
