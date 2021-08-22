f7a72b7435f1a392a65205b1e31f440e9a51996a
Alex <cupokoffi@gmail.com>
2021-08-22 13:01:11 -0400

    ll

--- src/Components/The-Gallery/The-Gallery/The-Gallery.js
+++ src/Components/The-Gallery/The-Gallery/The-Gallery.js
@@ -5,7 +5,49 @@ function TheGallery() {
     return (
         <div>
           <div className="The-Gallery">
-            <h1 className="welcome">The Gallery</h1>
+            <h1 className="the-gallery">The Gallery</h1> 
+            <hr /> 
+
+            <img id="image-thing-at" src="TG1.jpeg" /> 
+            <h5 id="caption">
+              Lancaster, Pennsylvania 
+            </h5>
+
+            <img id="image-thing-at" src="TG2.jpeg" /> 
+            <h5 id="caption">
+              Lancaster, Pennsylvania 
+            </h5>
+
+            <img id="image-thing-at" src="TG3.jpeg" /> 
+            <h5 id="caption">
+              Lancaster, Pennsylvania 
+            </h5>
+
+            <img id="image-thing-at" src="TG4.jpeg" /> 
+            <h5 id="caption">
+              Lancaster, Pennsylvania 
+            </h5>
+
+            <img id="image-thing-at" src="TG5.jpeg" /> 
+            <h5 id="caption">
+              Lancaster, Pennsylvania 
+            </h5>
+
+            <img id="image-thing-at" src="TG6.jpeg" /> 
+            <h5 id="caption">
+              Lancaster, Pennsylvania 
+            </h5>
+
+            <img id="image-thing-at" src="TG7.jpeg" /> 
+            <h5 id="caption">
+              Lancaster, Pennsylvania 
+            </h5>
+
+            <img id="image-thing-at" src="TG8.jpeg" /> 
+            <h5 id="caption">
+              Lancaster, Pennsylvania 
+            </h5> 
+
           </div>
         </div>
     );
