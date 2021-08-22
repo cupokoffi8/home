baf2126f632648afa5fcad13c4ebe68f3d9ea142
Alex <cupokoffi@gmail.com>
2021-08-22 11:54:51 -0400

    ll

--- src/Components/Exhibitions/Oil-Painting-Exhibition/Oil-Painting-Exhibition.js
+++ src/Components/Exhibitions/Oil-Painting-Exhibition/Oil-Painting-Exhibition.js
@@ -17,6 +17,8 @@ function PleaseWork() {
               <img id="image-thing-at-uhh" src="./OP2.jpeg" /> 
             </div> 
 
+            <br /> 
+
             <p id="art-paragraph-other">
             The famous curator, Lena Liu, said: The Oil Painting Exhibition of Zhao Qing, at Jeju Island, 
             South Korea,  attracted the attention of many collectors.  The famous artist Zhao Quing is the 
