5b44131b37b3944a8cd274c73a7510e524a06aee
Alex <cupokoffi@gmail.com>
2021-08-22 15:33:35 -0400

    ll

--- src/Components/Art-Service/Art-Service.js
+++ src/Components/Art-Service/Art-Service.js
@@ -1,11 +1,42 @@
 import React from 'react'; 
-import './Art-Service.css'; 
+import '../Artists/Zen-Frescos/Zen-Frescos.css';  
 
 function ArtService() {
     return (
         <div>
           <div className="Art-Service">
-            <h1 className="welcome">Art Service</h1>
+            <h1 className="artist">Art Service</h1> 
+            <hr /> 
+
+            <p id="art-paragraph-other">
+            The American Dragon Gallery has a strong team of art consultants. The consultants have senior qualifications 
+            related to various international art fields.  They try their best to recommend some of the most talented potential 
+            artists to display their works at The American Dragon Gallery.  In addition, our team helps artists establish 
+            cooperation opportunities with the world’s best galleries and auction venues, such as Sotheby’s, Christies, Art 
+            Basel, private collectors, and museums.  This professional team will help showcase the artist’s talents through 
+            exhibitions, seminars, transnational residency creations, and auctions.  We use our resources related to our global 
+            partner network to help distribute the high quality artwork.  American Dragon adopts an open, inclusive, and 
+            diversified attitude, attracting talented and creative artists from all walks of life.  Please contact us to learn 
+            more about our services. 
+            </p>
+
+            <br /> 
+
+            <p id="art-paragraph-other">
+            Phone: 917-886-1701
+            </p> 
+
+            <p id="art-paragraph-other">
+            Email: info@americandragonfineart.com 
+            </p> 
+
+            <hr /> 
+            <br /> 
+
+            <h1 className="artist">Chinese Art Tour</h1> 
+            
+
+
           </div>
         </div>
     );
