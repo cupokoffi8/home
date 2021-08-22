6354568eda4cef30d3f760b2bc5e78496b003a3d
Alex <cupokoffi@gmail.com>
2021-08-22 13:15:06 -0400

    ll

--- src/Components/The-Gallery/The-Gallery/The-Gallery.js
+++ src/Components/The-Gallery/The-Gallery/The-Gallery.js
@@ -15,38 +15,51 @@ function TheGallery() {
 
             <img id="image-thing-at" src="TG2.png" /> 
             <h5 id="caption">
-              Lancaster, Pennsylvania 
+              New York City, New York  
             </h5>
 
             <img id="image-thing-at" src="TG3.png" /> 
             <h5 id="caption">
-              Lancaster, Pennsylvania 
+              New York City, New York  
             </h5>
 
             <img id="image-thing-at" src="TG4.png" /> 
             <h5 id="caption">
-              Lancaster, Pennsylvania 
+              Hangzhou, China 
             </h5>
 
             <img id="image-thing-at" src="TG5.png" /> 
             <h5 id="caption">
-              Lancaster, Pennsylvania 
+              Beijing, China 
             </h5>
 
             <img id="image-thing-at" src="TG6.png" /> 
             <h5 id="caption">
-              Lancaster, Pennsylvania 
+              Japan 
             </h5>
 
             <img id="image-thing-at" src="TG7.jpeg" /> 
             <h5 id="caption">
-              Lancaster, Pennsylvania 
+              The Wall Street Art Space, New York, NY 
             </h5>
 
             <img id="image-thing-at" src="TG8.jpeg" /> 
             <h5 id="caption">
-              Lancaster, Pennsylvania 
+              The Wall Street Art Space, New York, NY 
             </h5> 
+
+            <br /> 
+
+            <p id="art-paragraph-other">
+            American Dragon International Fine Art is a leading source for showcasing some of the most talented artists 
+            from around the world. We help establish close cooperation opportunities between artists and some of the best 
+            art galleries and auction venues globally, such as Sotheby's, Christie's, Basel Art Fair, and private collector 
+            museums. Our professional knowledge will help showcase the artistic talent in both private and public venues 
+            through exhibitions, seminars, agencies and auctions. The artist's high quality pieces are launched using the 
+            resources we offer through our global partnership network. American Dragon adopts an open, inclusive and diverse 
+            attitude that draws talented and creative artists from all walks of life. Please contact us to find out more 
+            about our services. 
+            </p>
 
           </div>
         </div>
