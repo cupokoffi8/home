879842d2db173ed726bf992dd35d417d77137cd2
Alex <cupokoffi@gmail.com>
2021-08-22 20:13:24 -0400

    ll

--- src/Components/Artists/Aparna-Banerjee/Aparna-Banerjee.js
+++ src/Components/Artists/Aparna-Banerjee/Aparna-Banerjee.js
@@ -31,6 +31,81 @@ function AparnaBanerjee() {
               <img src="./beauty_2.jpeg" id="painting"></img> 
               <h5 id="caption">Beauty 2</h5> 
 
+              <img src="./AB3.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 3</h5> 
+
+              <img src="./AB4.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 4</h5> 
+
+              <img src="./AB5.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 5</h5> 
+
+              <img src="./AB6.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 6</h5> 
+
+              <img src="./AB7.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 7</h5> 
+
+              <img src="./AB8.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 8</h5> 
+
+              <img src="./AB9.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 9</h5> 
+
+              <img src="./AB10.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 10</h5> 
+
+              <img src="./AB11.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 11</h5> 
+
+              <img src="./AB12.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 12</h5> 
+
+              <img src="./AB13.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 13</h5> 
+
+              <img src="./AB14.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 14</h5> 
+
+              <img src="./AB15.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 15</h5> 
+
+              <img src="./AB16.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 16</h5> 
+
+              <img src="./AB17.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 17</h5> 
+
+              <img src="./AB18.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 18</h5> 
+
+              <img src="./AB19.jpeg" id="painting"></img> 
+              <h5 id="caption">Beauty 19</h5> 
+
+              <img src="./F1.jpeg" id="painting"></img> 
+              <h5 id="caption">Flowers 1</h5> 
+
+              <img src="./F2.jpeg" id="painting"></img> 
+              <h5 id="caption">Flowers 2</h5> 
+
+              <img src="./F3.jpeg" id="painting"></img> 
+              <h5 id="caption">Flowers 3</h5> 
+
+              <img src="./F4.jpeg" id="painting"></img> 
+              <h5 id="caption">Flowers 4</h5> 
+
+              <img src="./F5.jpeg" id="painting"></img> 
+              <h5 id="caption">Flowers 5</h5> 
+
+              <img src="./F6.jpeg" id="painting"></img> 
+              <h5 id="caption">Flowers 6</h5> 
+
+              <img src="./F7.jpeg" id="painting"></img> 
+              <h5 id="caption">Flowers 7</h5> 
+
+              <img src="./Maiden-in-Color.jpeg" id="painting"></img> 
+              <h5 id="caption">Maiden in Color</h5> 
+
           </div>
     );
   }
