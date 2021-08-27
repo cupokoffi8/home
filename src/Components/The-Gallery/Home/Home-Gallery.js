8f93ab298c32379e7eac91f16be07c2a1f5f571d
Alex <cupokoffi@gmail.com>
2021-08-27 11:50:47 -0400

    ll

--- src/Components/The-Gallery/Home/Home-Gallery.js
+++ src/Components/The-Gallery/Home/Home-Gallery.js
@@ -1,12 +1,14 @@
 import React from 'react'; 
 import './Home-Gallery.css'; 
 import Navbar from '../../Navbar/Navbar'; 
+import '../../Button.css'; 
 
 function HomeGallery() {
     return (
       <>
         <Navbar />
           <div className="Home-Gallery">
+          <button id="the-button" href="">普通話</button>
             <h1 className="welcome">Home</h1>
           </div>
         </> 
