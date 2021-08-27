6eb0428bdccc4dfd9916d85d997c361454dba148
Alex <cupokoffi@gmail.com>
2021-08-27 16:20:50 -0400

    ll

--- src/Components/Navbar-Mandarin/A-MenuItems.js
+++ src/Components/Navbar-Mandarin/A-MenuItems.js
@@ -1,52 +1,52 @@
 export const AMenuItems = [
     {
       title: 'Aparna Banerjee',
-      path: '/aparna-banerjee',
+      path: '/aparna-banerjee-mandarin',
       cName: 'a-dropdown-link'
     },
     {
       title: 'Apelles Zhou',
-      path: '/apelles-zhou',
+      path: '/apelles-zhou-mandarin',
       cName: 'a-dropdown-link'
     },
     {
       title: 'Cai Jiang',
-      path: '/cai-jiang',
+      path: '/cai-jiang-mandarin',
       cName: 'a-dropdown-link'
     }, 
     {
       title: 'Chiu Pai',
-      path: '/chiu-pai',
+      path: '/chiu-pai-mandarin',
       cName: 'a-dropdown-link'
     }, 
     {
       title: 'Qui Huade',
-      path: '/qui-huade',
+      path: '/qui-huade-mandarin',
       cName: 'a-dropdown-link'
     }, 
     {
       title: 'Su Zi',
-      path: '/su-zi',
+      path: '/su-zi-mandarin',
       cName: 'a-dropdown-link'
     }, 
     {
       title: 'Xiao Li',
-      path: '/xiao-li',
+      path: '/xiao-li-mandarin',
       cName: 'a-dropdown-link'
     }, 
     {
       title: 'Zen Frescos',
-      path: '/zen-frescos',
+      path: '/zen-frescos-mandarin',
       cName: 'a-dropdown-link'
     }, 
     {
       title: 'Zhao Qing',
-      path: '/zhao-qing',
+      path: '/zhao-qing-mandarin',
       cName: 'a-dropdown-link'
     }, 
     {
       title: 'Zhen Zhong Duan',
-      path: '/zhen-zhong-duan',
+      path: '/zhen-zhong-duan-mandarin',
       cName: 'a-dropdown-link'
     }, 
   ];
