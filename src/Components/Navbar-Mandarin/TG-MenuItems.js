17c550aa0d111f2348973824fa15d1381caf4349
Alex <cupokoffi@gmail.com>
2021-08-27 14:29:51 -0400

    ll

--- src/Components/Navbar-Mandarin/TG-MenuItems.js
+++ src/Components/Navbar-Mandarin/TG-MenuItems.js
@@ -1,12 +1,12 @@
 export const TGMenuItems = [
     {
-      title: 'Home CHHHHINESE',
-      path: '/',
+      title: '主页', 
+      path: '/mandarin',
       cName: 'tg-dropdown-link'
     },
     {
-      title: 'Founders Column',
-      path: '/our-founder',
+      title: '创始人专栏', 
+      path: '/our-founder-mandarin', 
       cName: 'tg-dropdown-link'
     } 
   ];
