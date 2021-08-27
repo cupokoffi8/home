266054889866bb7047b047ffb2d579d1f65e8e2c
Alex <cupokoffi@gmail.com>
2021-08-27 12:54:19 -0400

    ll

--- src/Components/Home/Home-Mandarin.js
+++ src/Components/Home/Home-Mandarin.js
@@ -20,10 +20,10 @@ export default function HomeMandarin() {
       <button id="the-button" type='btn' href="/home/#/mag-and-myst">English</button> 
       </form> 
       
-        <h1 className="magAndMyst">"Magnificent And Mysterious"</h1>
-        <h1 clessName="currentExhibition" id="please">Art Exhibition</h1>
-        <h1 className='welcome' id="uh">Welcome To Uh Chinese</h1>
-        <h1 className='american'>American Dragon International Fine Art Gallery</h1>
+        <h1 className="magAndMyst">"宏伟而神秘"</h1>
+        <h1 clessName="currentExhibition" id="please">艺术展</h1>
+        <h1 className='welcome' id="uh">歡迎來到美國龍國際美術</h1>
+        {/* <h1 className='american'>American Dragon International Fine Art Gallery</h1> */} 
 
         <div id="info">
           <a id="addy-click" href="https://www.google.com/maps/place/American+Dragon+International+Fine+Art+Gallery/@40.0483338,-76.3059007,15z/data=!4m5!3m4!1s0x0:0x4c549ed6537c2a4!8m2!3d40.0483496!4d-76.3057639">
@@ -63,25 +63,14 @@ export default function HomeMandarin() {
           <img src="./fung-shway.jpeg"id="fung-shway" />  
 
           <p id="p-top">
-          American Dragon International Fine Art has partnerships with galleries in a wide range of 
-          cities and countries. Our founder, Lena Liu, through years of experience, realized that different 
-          countries and regions have various artistic styles and tastes in art. Some works of art do not 
-          have a positive acceptance in a given region, but the same art would be considered an exceptional 
-          creation in another area of the world. This can diminish the interest of an artist’s talents, and 
-          as a result, regional exhibitions can be a weak venue for the artist. In order to obtain a strong 
-          interest in the art, we have our galleries located in China, Japan, Korea, Italy, France, the 
-          United Kingdom, and the United States. The museums and galleries of many cities located in these 
-          countries, have established a long term relationship with American Dragon. This allows the artist 
-          to have the ability to showcase their creative talent through our professional partnerships.
-          </p>
-
-          <p id="p-bottom">
-          We also offer many other services to our artists and those interested in the arts in China and the 
-          United States. Artist visits, summer camps, art education, and tourism coordination are some of the 
-          various services we can provide. Each service provided to the artist would be customized for the 
-          artist’s individual exhibition and other business collaborations. Please contact us for additional 
-          information today! 
-          </p>
+          美国龙国际艺术与许多城市和国家的画廊都有合作伙伴关系。我们的创始人刘娜（Lena Liu）通过多年的经验认识到，
+          不同的国家和地区具有不同的艺术风格和品味。有些艺术作品在某些地区未被积极接受，但是同一艺术在世界其他地区将被视为非凡的创作。
+          这可能会降低艺术家才华的兴趣，因此区域性展览对于艺术家来说是一个薄弱的地方。为了对艺术产生浓厚的兴趣，我们在中国，日本，
+          韩国，意大利，法国，英国和美国设有画廊。这些国家/地区中许多城市的博物馆和美术馆与美国龙美术馆建立了长期的合作关系。
+          通过我们的专业合作伙伴关系，艺术家可以展示自己的创造才能。   我们还为我们的艺术家以及对中国和美国艺术感兴趣的人提供许多其他服务。
+          我们可以提供各种服务，包括艺术家参观，夏令营，艺术教育和旅游协调。为艺术家提供的每项服务都将根据艺术家的个人展览和其他业务合作进行定制。
+          请立即与我们联系以获取更多信息！
+          </p> 
         </div>
         
       </>
