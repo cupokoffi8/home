7c98acf54ac50eb3fe7fb3db397b7068f2998198
Alex <cupokoffi@gmail.com>
2021-09-03 14:46:29 -0400

    ll

--- src/Components/Artists/Jill-Slaymaker/Jill-Slaymaker.js
+++ src/Components/Artists/Jill-Slaymaker/Jill-Slaymaker.js
@@ -32,37 +32,37 @@ function JillSlaymaker() {
 
               <img src="./JS3.jpg" id="painting-zf"></img>
               <h5 id="caption">Toscana Spiritos</h5> 
-              <h5 id="caption">26" x 34"</h5> 
+              <h5 id="caption">26" x 34" [Framed]</h5> 
               <h5 id="caption">Oil and Acrylic on Paper</h5>
               <h5 id="caption">$1,800</h5> 
 
               <img src="./JS4.jpg" id="painting-zf"></img>
               <h5 id="caption">When I'm Through with This World, I'll Remember You</h5>
-              <h5 id="caption">13.5" x 10.5"</h5>
-              <h5 id="caption">Ink, Color Pencil, White Charcoal on Paper</h5> 
+              <h5 id="caption">10" x 13" [Framed]</h5>
+              <h5 id="caption">Ink, Gouache, Color Pencil, White Charcoal on Paper</h5> 
               <h5 id="caption">$500</h5> 
 
               <img src="./JS5.jpg" id="painting-zf"></img>
               <h5 id="caption">My Greedy Eyes Still Wandered</h5>
-              <h5 id="caption">10" x 7"</h5>
-              <h5 id="caption">Oil on Paper</h5> 
+              <h5 id="caption">10" x 13" [Framed]</h5>
+              <h5 id="caption">Oil, India Ink on Paper</h5> 
               <h5 id="caption">$500</h5> 
 
               <img src="./JS6.jpg" id="painting-zf"></img>
-              <h5 id="caption">Untitled</h5>
-              <h5 id="caption">26" x 34"</h5>
-              <h5 id="caption">Blue, Oil, Acrylic, Watercolor, White Charcoal on Paper</h5> 
+              <h5 id="caption">Blue</h5>
+              <h5 id="caption">34" x 26" [Framed]</h5>
+              <h5 id="caption">Gouache, Oil, Acrylic, Watercolor, White Charcoal on Paper</h5> 
               <h5 id="caption">$1,600</h5> 
 
               <img src="./JS7.jpg" id="painting-zf"></img>
               <h5 id="caption">Enter the New World</h5>
-              <h5 id="caption">12" x 9"</h5>
-              <h5 id="caption">Acrylic, Ink on Paper</h5> 
+              <h5 id="caption">10" x 13" [Framed]</h5>
+              <h5 id="caption">Gouache, Acrylic, Ink, Color Pencil on Paper</h5> 
               <h5 id="caption">$500</h5> 
 
               <img src="./JS8.jpg" id="painting-zf"></img>
-              <h5 id="caption">Adventures of an Orange</h5>
-              <h5 id="caption">34" x 26"</h5>
+              <h5 id="caption">Adventures of an Orange</h5> 
+              <h5 id="caption">34" x 26" [Framed]</h5>
               <h5 id="caption">Gouache on Paper</h5> 
               <h5 id="caption">$1,600</h5> 
 
