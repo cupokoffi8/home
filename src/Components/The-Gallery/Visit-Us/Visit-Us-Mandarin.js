fff78125753d4a67a53eb72fd42b3d5c82f7b0f7
Alex <cupokoffi@gmail.com>
2021-09-08 11:53:36 -0400

    ll

--- src/Components/The-Gallery/Visit-Us/Visit-Us-Mandarin.js
+++ src/Components/The-Gallery/Visit-Us/Visit-Us-Mandarin.js
@@ -18,19 +18,23 @@ function VisitUsMandarin() {
             <h1 className="the-gallery">关于我们</h1> 
             <hr /> 
 
+            <div data-aos="fade-up" data-aos-duration="900">
             <Link id="link" to={`/the-gallery-mandarin`}>
               <Card  className="the-card">
                 <Card.Img src="./photo-frame.png" variant="top" id="image-thing-vu"/>
               </Card>
               <Card.Title className="cardTitle">画廊</Card.Title> 
               </Link>
+              </div> 
 
+              <div data-aos="fade-up" data-aos-duration="1000">
               <Link id="link" to={`/our-founder-mandarin`}>
               <Card  className="the-card">
                 <Card.Img src="./ceo.png" variant="top" id="image-thing-vu-2"/>
               </Card>
               <Card.Title className="cardTitle">我們的創始人</Card.Title> 
-              </Link>
+              </Link> 
+              </div> 
 
           </div> 
         </> 
