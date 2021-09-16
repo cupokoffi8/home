1fcdfd40e21795ccc5a366b3d30554ab11f576bb
Alex <cupokoffi@gmail.com>
2021-09-16 19:39:12 -0400

    About Us

--- src/Components/The-Gallery/Visit-Us/Visit-Us-Mandarin.js
+++ src/Components/The-Gallery/Visit-Us/Visit-Us-Mandarin.js
@@ -18,27 +18,28 @@ function VisitUsMandarin() {
             <h1 className="the-gallery">关于我们</h1> 
             <hr /> 
 
-            <div data-aos="fade-up" data-aos-duration="900">
-            <Link id="link" to={`/the-gallery-mandarin`}>
-              <Card  className="the-card">
+            <div className="card-row">
+            <div data-aos="fade-up" data-aos-duration="900" className="the-gallery-card">
+            <Link data-aos="fade-up" data-aos-duration="900" id="link" to={`/the-gallery`}>
+              <Card className="the-card">
                 <Card.Img src="./photo-frame.png" variant="top" id="image-thing-vu"/>
               </Card>
               <Card.Title className="cardTitle">画廊</Card.Title> 
               </Link>
               </div> 
-
-              <div data-aos="fade-up" data-aos-duration="1000">
-              <Link id="link" to={`/our-founder-mandarin`}>
-              <Card  className="the-card">
+ 
+              <div data-aos="fade-up" data-aos-duration="1000" className="our-founder-card"> 
+              <Link id="link" to={`/our-founder`}>
+              <Card className="the-card">
                 <Card.Img src="./ceo.png" variant="top" id="image-thing-vu-2"/>
               </Card>
               <Card.Title className="cardTitle">我們的創始人</Card.Title> 
               </Link> 
               </div> 
-
+            </div> 
           </div> 
         </> 
     );
   }
   
-  export default VisitUsMandarin;
+  export default VisitUsMandarin; 
--- src/Components/The-Gallery/Visit-Us/Visit-Us.js
+++ src/Components/The-Gallery/Visit-Us/Visit-Us.js
@@ -21,7 +21,8 @@ function VisitUs() {
             <h1 className="the-gallery">About Us</h1> 
             <hr /> 
 
-            <div data-aos="fade-up" data-aos-duration="900" >
+          <div className="card-row">
+            <div data-aos="fade-up" data-aos-duration="900" className="the-gallery-card">
             <Link data-aos="fade-up" data-aos-duration="900" id="link" to={`/the-gallery`}>
               <Card className="the-card">
                 <Card.Img src="./photo-frame.png" variant="top" id="image-thing-vu"/>
@@ -30,7 +31,7 @@ function VisitUs() {
               </Link>
               </div> 
  
-              <div data-aos="fade-up" data-aos-duration="1000">
+              <div data-aos="fade-up" data-aos-duration="1000" className="our-founder-card"> 
               <Link id="link" to={`/our-founder`}>
               <Card className="the-card">
                 <Card.Img src="./ceo.png" variant="top" id="image-thing-vu-2"/>
@@ -38,6 +39,7 @@ function VisitUs() {
               <Card.Title className="cardTitle">Our Founder</Card.Title> 
               </Link> 
               </div> 
+            </div> 
 
           </div>
         </> 
