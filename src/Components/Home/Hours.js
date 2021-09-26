a81704d4238cea96fe134f3c63c93ebea8806e59
Alex <cupokoffi@gmail.com>
2021-09-26 11:15:22 -0400

    ll

--- src/Components/Home/Hours.js
+++ src/Components/Home/Hours.js
@@ -15,12 +15,12 @@ function Hours() {
           <div id="space-uh" />
             <h6 data-aos="fade-up" data-aos-duration="1100" className="hours">
               Monday: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; By Appointment Only<br /> 
-              Tuesday: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; By Appointment Only<br />
+              Tuesday: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; By Appointment Only<br />
               Wednesday: &nbsp;&nbsp; By Appointment Only<br />
               Thursday:  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Noon - 7:00 p.m.<br />
               Friday: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Noon - 7:00 p.m.<br /> 
-              Saturday: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 10:00 a.m. - 5:00 p.m.<br />
-              Sunday: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Closed</h6> 
+              Saturday: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 10:00 a.m. - 5:00 p.m.<br />
+              Sunday: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Closed</h6> 
             <div id="space" />
         </div>
     );
