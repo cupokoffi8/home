301b4c439f13eb1e79207a6ef8b7badedd9ec130
Alex <cupokoffi@gmail.com>
2022-03-24 14:17:37 -0400

    Revise Copyright

--- src/Copyright.jsx
+++ src/Copyright.jsx
@@ -3,6 +3,6 @@ import './Copyright.css';
 
 export default function Copyright() {
     return (
-        <p id="copyright"> American Dragon International Fine Art © Copyright 2021 </p> 
+        <p id="copyright"> American Dragon International Fine Art © Copyright 2022 </p> 
     )
 }
