84613c7e6962a0e8ad89b6836318e995d280a60a
Alex <cupokoffi@gmail.com>
2022-05-24 10:06:02 -0400

    Appoinments

--- src/Components/Home/Visit-Mandarin.jsx
+++ src/Components/Home/Visit-Mandarin.jsx
@@ -8,8 +8,8 @@ function VisitMandarin() {
 
     return (
         <div id="visit"> 
-          <h1 className="sub">探望我们</h1> 
-          <h3 className="paragraph">我们将接受预约，每天上午 10:00 至晚上 8:00 不按以下时间表参观我们的画廊。周日至周六。</h3> 
+          <h1 className="sub">探望我们</h1>   
+          <h3 className="paragraph">我们将接 <a id="apt" href="https://www.facebook.com/americandragonart/services/">受预约</a>，每天上午 10:00 至晚上 8:00 不按以下时间表参观我们的画廊。周日至周六。</h3> 
           <div className="the-hours-container"> 
                 <h3 className="a-head">2022 年 5 月开放时间</h3>
                 <div className="days">
--- src/Components/Home/Visit.jsx
+++ src/Components/Home/Visit.jsx
@@ -9,7 +9,7 @@ function Visit() {
     return (
         <div id="visit"> 
           <h1 className="sub">Visit Us</h1> 
-          <h3 className="paragraph">We will accept appointments to visit our gallery every day that is not on 
+          <h3 className="paragraph">We will accept <a id="apt" href="https://www.facebook.com/americandragonart/services/">appointments</a> to visit our gallery every day that is not on 
               the schedule below, from 10:00 a.m. until 8:00 p.m. Sunday through Saturday. </h3> 
           <div className="the-hours-container"> 
                 <h3 className="a-head">May 2022 Open Hours</h3>
