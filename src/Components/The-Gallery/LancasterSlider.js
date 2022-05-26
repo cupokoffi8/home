e09d360cc351fb5f57703b835bf56d8551ca36c6
Alex <cupokoffi@gmail.com>
2022-05-26 09:12:24 -0400

    Social Media icon css

--- src/Components/The-Gallery/LancasterSlider.js
+++ src/Components/The-Gallery/LancasterSlider.js
@@ -31,7 +31,7 @@ const ImageSlider = ({ slides }) => {
             key={index}
           >
             {index === current && ( 
-              <img src={slide.image} alt='travel image' className='image' /> 
+              <img src={slide.image} alt='gallery image' className='image' /> 
             )}
           </div>
         );
