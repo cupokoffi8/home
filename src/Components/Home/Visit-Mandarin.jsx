2cc016786737dbc19ba785a98c91750e1d8fa8db
Alex <cupokoffi@gmail.com>
2022-05-29 16:07:20 -0400

    Update hours

--- src/Components/Home/Visit-Mandarin.jsx
+++ src/Components/Home/Visit-Mandarin.jsx
@@ -11,17 +11,15 @@ function VisitMandarin() {
           <h1 className="sub">探望我们</h1>   
           <h3 className="paragraph">我们将接 <a id="apt" href="https://www.facebook.com/americandragonart/services/">受预约</a>，每天上午 10:00 至晚上 8:00 不按以下时间表参观我们的画廊。周日至周六。</h3> 
           <div className="the-hours-container"> 
-                <h3 className="a-head">2022 年 5 月开放时间</h3>
+                <h3 className="a-head">即将开放的时间</h3>
                 <div className="days">
                   <h2 className="days-header">
-                  5 月 27 日星期五：<br />
-                  5月28日星期六：
+                  6 月 3 日星期五：<br />
                   </h2> 
                 </div>
                 <div className="the-hours">
                   <h2 className="hours-header">
-                  下午 2:00 - 晚上 7:00<br />
-                  中午 - 下午 5:00
+                  下午 4:00 - 晚上 9:00<br />
                   </h2>
                 </div>
               </div>
--- src/Components/Home/Visit.jsx
+++ src/Components/Home/Visit.jsx
@@ -12,17 +12,15 @@ function Visit() {
           <h3 className="paragraph">We will accept <a id="apt" href="https://www.facebook.com/americandragonart/services/">appointments</a> to visit our gallery every day that is not on 
               the schedule below, from 10:00 a.m. until 8:00 p.m. Sunday through Saturday. </h3> 
           <div className="the-hours-container"> 
-                <h3 className="a-head">May 2022 Open Hours</h3>
+                <h3 className="a-head">Upcoming Open Hours</h3>
                 <div className="days">
                   <h2 className="days-header">
-                  Friday, May 27th: <br />
-                  Saturday, May 28th: 
+                  'First' Friday, June 3rd: <br />
                   </h2> 
                 </div>
                 <div className="the-hours">
                   <h2 className="hours-header">
-                  2:00 p.m. - 7:00 p.m.<br /> 
-                  Noon - 5:00 p.m. 
+                  4:00 p.m. - 9:00 p.m.<br /> 
                   </h2>
                 </div>
               </div>
