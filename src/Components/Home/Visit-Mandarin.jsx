af1255f36fd8ae1766a2bdbdd2e19fa84536bb4b
Alex <cupokoffi@gmail.com>
2022-06-13 20:53:16 -0400

    Update hours

--- src/Components/Home/Visit-Mandarin.jsx
+++ src/Components/Home/Visit-Mandarin.jsx
@@ -14,14 +14,12 @@ function VisitMandarin() {
                 <h3 className="a-head">即将开放的时间</h3>
                 <div className="days">
                   <h2 className="days-header">
-                  6 月 10 日星期五：<br />
-                  6 月 11 日星期六：<br /> 
+                  6月18日星期六：<br /> 
                   </h2> 
                 </div>
                 <div className="the-hours">
                   <h2 className="hours-header"> 
-                  下午 4:00 - 晚上 7:00<br />
-                  上午 10:00 - 下午 2:00<br /> 
+                  上午 10:00 - 下午 3:00<br /> 
                   </h2>
                 </div>
               </div>
--- src/Components/Home/Visit.jsx
+++ src/Components/Home/Visit.jsx
@@ -9,20 +9,18 @@ function Visit() {
     return (
         <div id="visit"> 
           <h1 className="sub">Visit Us</h1> 
-          <h3 className="paragraph">We will accept <a id="apt" href="https://www.facebook.com/americandragonart/services/">appointments</a> to visit our gallery every day that is not on 
+          <h3 className="paragraph">We will accept <a id="apt" href="https://www.facebook.com/americandragonart/services/">appointments (Via Facebook)</a>, or call us to visit our gallery every day that is not on 
               the schedule below, from 10:00 a.m. until 8:00 p.m. Sunday through Saturday. </h3> 
           <div className="the-hours-container"> 
                 <h3 className="a-head">Upcoming Open Hours</h3>
                 <div className="days">
                   <h2 className="days-header">
-                  Friday, June 10th: <br />
-                  Saturday, June 11th: <br />
+                  Saturday, June 18th: <br />
                   </h2> 
                 </div>
                 <div className="the-hours">
                   <h2 className="hours-header">
-                  4:00 PM - 7:00 PM<br />
-                  10:00 AM - 2:00 PM<br /> 
+                  10:00 AM - 3:00 PM<br /> 
                   </h2>
                 </div>
               </div>
