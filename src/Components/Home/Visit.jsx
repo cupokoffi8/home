8b7b0c65793a0e20510037faa18f2b880407a476
Alex <cupokoffi@gmail.com>
2022-06-20 22:14:21 -0400

    Update hours

--- src/Components/Home/Visit.jsx
+++ src/Components/Home/Visit.jsx
@@ -12,15 +12,18 @@ function Visit() {
           <h3 className="paragraph">We will accept <a id="apt" href="https://www.facebook.com/americandragonart/services/">Appointments (Via Facebook)</a>, or call us to visit our gallery every day that is not on 
               the schedule below, from 10:00 a.m. until 8:00 p.m. Sunday through Saturday. </h3> 
           <div className="the-hours-container"> 
-                <h3 className="a-head">Recent Open Hours</h3>
+                {/* Change to "Recent Open Hours" when upcoming hours are not known */}
+                <h3 className="a-head">Upcoming Open Hours</h3> 
                 <div className="days">
-                  <h2 className="days-header">
-                  Saturday, June 18th: <br />
+                  <h2 className="days-header"> 
+                  Friday, June 24th: <br /> 
+                  Saturday, June 25th: <br />
                   </h2> 
                 </div>
                 <div className="the-hours">
                   <h2 className="hours-header">
-                  10:00 AM - 3:00 PM<br /> 
+                  4:00 PM - 7:00 PM<br /> 
+                  10:00 AM - 2:00 PM<br /> 
                   </h2>
                 </div>
               </div>
