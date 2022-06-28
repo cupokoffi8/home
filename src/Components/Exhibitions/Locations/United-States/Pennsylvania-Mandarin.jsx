48368e22aa9be7adf7ff700efefc611a3b8aca04
Alex <cupokoffi@gmail.com>
2022-06-28 10:40:24 -0400

    Exhibition routing [M]

--- src/Components/Exhibitions/Locations/United-States/Pennsylvania-Mandarin.jsx
+++ src/Components/Exhibitions/Locations/United-States/Pennsylvania-Mandarin.jsx
@@ -21,21 +21,21 @@ function PennsylvaniaMandarin() {
             {/* href began with: /home/#/ because of gh-pages, but was changed because of domain name change */} 
 
             <div className="card-row-artists"> 
-            <a id="link" href="#/ink-storm">
+            <a id="link" href="#/ink-storm-mandarin">
             <h5 id="caption-card-l">墨风暴</h5> 
             </a> 
-            <a id="link" href="#/mag-and-myst">
+            <a id="link" href="#/mag-and-myst-mandarin">
             <h5 id="caption-card-r">“華麗與神秘”藝術展</h5> 
             </a>
             </div> 
 
             {/* Mobile */} 
  
-            <a id="link" href="#/ink-storm"> 
+            <a id="link" href="#/ink-storm-mandarin"> 
             <h5 id="caption-card">墨风暴</h5> 
             </a> 
 
-            <a id="link" href="#/mag-and-myst"> 
+            <a id="link" href="#/mag-and-myst-mandarin"> 
             <h5 id="caption-card">“華麗與神秘”藝術展</h5> 
             </a> 
 
